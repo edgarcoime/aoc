9510478648a2173083a25a32518de1ddf6aa3742
Edgar Coime <ecoime.dev@gmail.com>
2026-03-13 10:36:31 -0700

    feat: setup rust day01/2021

---
+++ 2021/src/bin/day01.rs
@@ -0,0 +1,10 @@
+use aoc::read_input;
+
+fn parse(input: &str) {}
+
+fn main() {
+    let input = read_input("examples/day01_p1.txt");
+    let input2 = read_input("inputs/day01_p1.txt");
+    println!("{input}");
+    println!("\n{input2}");
+}
---
+++ 2021/src/lib.rs
@@ -0,0 +1,6 @@
+use std::fs;
+use std::path::Path;
+
+pub fn read_input<P: AsRef<Path>>(path: P) -> String {
+    fs::read_to_string(path).expect("Failed to read input file")
+}
