81f4938f3d742e751af0ff40a40b6ec125cd1fca
Edgar Coime <ecoime.dev@gmail.com>
2026-03-13 12:19:16 -0700

    feat: da1/p2/2021 done

--- 2021/src/bin/day01.rs
+++ 2021/src/bin/day01.rs
@@ -26,15 +26,38 @@ fn part1(input: &str) -> i32 {
 }
 
 fn part2(input: &str) -> i32 {
-    0
+    let nums = parse(&input);
+    nums
+        .windows(3)
+        .map(|win| win
+            .iter()
+            .fold(0, |acc, el| el + acc)
+        )
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .fold(0, |acc, win| {
+            match win {
+                [a, b] => {
+                    if a < b {
+                        acc + 1
+                    } else {
+                        acc
+                    }
+                }
+                _ => panic!("unexpected window size")
+            }
+        })
 }
 
 fn main() {
-    let input = read_input("inputs/day01_p1.txt");
+    let input1 = read_input("inputs/day01_p1.txt");
+    let input2 = read_input("inputs/day01_p1.txt");
 
-    let p1 = part1(&input);
+    let p1 = part1(&input1);
+    let p2 = part2(&input2);
 
     println!("part 1: {}", p1);
+    println!("part 2: {}", p2);
 }
 
 #[cfg(test)]
@@ -46,5 +69,11 @@ mod tests {
     fn test_part1_sample() {
         let input = read_input("examples/day01_p1.txt");
         assert_eq!(part1(&input), 7)
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        let input = read_input("examples/day01_p1.txt");
+        assert_eq!(part2(&input), 5)
     }
 }
