aacd978938e69b36e114ca0c98736bd2d88e1457
Edgar Coime <ecoime.dev@gmail.com>
2026-03-13 11:57:17 -0700

    feat: d1/p1/2021 done

--- 2021/src/bin/day01.rs
+++ 2021/src/bin/day01.rs
@@ -1,10 +1,50 @@
 use aoc::read_input;
 
-fn parse(input: &str) {}
+fn parse(input: &str) -> Vec<i32> {
+    input
+        .lines()
+        .map(|l| l.trim().parse::<i32>().expect("input is not a number"))
+        .collect()
+}
+
+fn part1(input: &str) -> i32 {
+    let nums = parse(&input);
+    nums
+        .windows(2)
+        .fold(0, |acc, win| {
+            match win {
+                [a, b] => {
+                    if a < b {
+                        acc + 1
+                    } else {
+                        acc
+                    }
+                }
+                _ => panic!("unexpected window size")
+            }
+        })
+}
+
+fn part2(input: &str) -> i32 {
+    0
+}
 
 fn main() {
-    let input = read_input("examples/day01_p1.txt");
-    let input2 = read_input("inputs/day01_p1.txt");
-    println!("{input}");
-    println!("\n{input2}");
+    let input = read_input("inputs/day01_p1.txt");
+
+    let p1 = part1(&input);
+
+    println!("part 1: {}", p1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aoc::read_input;
+
+    #[test]
+    fn test_part1_sample() {
+        let input = read_input("examples/day01_p1.txt");
+        assert_eq!(part1(&input), 7)
+    }
 }
